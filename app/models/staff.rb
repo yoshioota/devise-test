0489fc6eb1b32b21826b72a30588cbc783308428
yoshi oota <yoshi.oota@gmail.com>
2014-10-25 15:33:45 +0900

    first commit

---
+++ app/models/staff.rb
@@ -0,0 +1,14 @@
+class Staff < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise(
+      :database_authenticatable,
+      :registerable,
+      :recoverable,
+      :rememberable,
+      :trackable,
+      :validatable,
+  )
+
+
+end
